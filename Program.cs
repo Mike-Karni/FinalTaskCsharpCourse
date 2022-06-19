afd720114c4f4b042f0b0d225d1d39e715e4f1af
mike_karni <3923499@mail.ru>
2022-06-19 18:50:27 +0300

    CreatedReadmeFile

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
-﻿string[] array = new string[]{"123", "23", "hello", "world", "res"};
+﻿
+
+string[] array = new string[]{"123", "23", "hello", "world", "res"};
 string[] newArray = new string[array.Length];
+int newSize = 3;
+
+// Main Method which 
+
 void ArrayFiltered(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+    if(array1[i].Length <= newSize)
         {
         array2[count] = array1[i];
         count++;
         }
     }
 }
+
+// Created Method Print Array
+
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
