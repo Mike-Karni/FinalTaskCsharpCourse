12751418f2f3ab75af49817d49ddbb8fd265dd34
mike_karni <3923499@mail.ru>
2022-06-19 18:47:38 +0300

    FixedDiagram

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[]{"123", "23", "hello", "world", "res"};
+string[] newArray = new string[array.Length];
+void ArrayFiltered(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayFiltered(array, newArray);
+PrintArray(newArray);
